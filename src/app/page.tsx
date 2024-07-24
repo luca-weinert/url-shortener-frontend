fbf198c76266503a10d58764231e5efd87f7d78d
Luca Weinert <lweinert@igus.net>
2024-07-24 22:57:00 +0200

    feat: gooogle cloud test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,6 +9,7 @@ export default function Home() {
 
     return (
         <>
+            <p>Goooogle</p>
             <div className="container mx-auto mt-8 bg">
                 <h1 className="text-center font-extrabold text-3xl mb-3.5">Basic URL Shortener</h1>
                 <div className="bg-gray-700 p-5 border-1 rounded-3xl shadow-md border-gray-200" >
